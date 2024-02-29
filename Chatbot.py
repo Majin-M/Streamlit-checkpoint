126ed27aa89851e98bd7035dbb0f288c45b3d601
Majin-M <81118856+Majin-M@users.noreply.github.com>
2024-02-29 14:01:12 +0000

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,9 @@
 
 import streamlit as st
 import nltk
+import epub2txt
+
+from epub2txt import epub2txt
 nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -23,36 +26,42 @@ def preprocess(text):
     
     return preprocessed_sentences
 
+
+# Définir la fonction du chatbot
+def chatbot(query, preprocessed_sentences):
+    if not preprocessed_sentences:
+        return "Désolé, je ne dispose pas de données pour répondre à votre question."
+
+    most_relevant_sentence = get_most_relevant_sentence(query, preprocessed_sentences)
+    return most_relevant_sentence
 # Définir la fonction de similarité
 def get_most_relevant_sentence(query, preprocessed_sentences):
-    # Transformer la requête en vecteur tf-idf
+    if not preprocessed_sentences:
+        return ""
+
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(preprocessed_sentences + [query])
-    
-    # Calculer la similarité cosinus entre le vecteur tf-idf de la requête et chaque vecteur tf-idf de phrase
+
     similarities = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1])[0]
-    
-    # Obtenir l'index de la phrase la plus similaire
+
     most_similar_idx = similarities.argmax()
     
     return preprocessed_sentences[most_similar_idx]
 
-# Définir la fonction du chatbot
-def chatbot(query, preprocessed_sentences):
-    most_relevant_sentence = get_most_relevant_sentence(query, preprocessed_sentences)
-    # Vous pouvez retourner la phrase la plus pertinente comme réponse pour plus de simplicité
-    return most_relevant_sentence
 
-# Créer une application Streamlit
+def extract_text_from_epub(epub_file):
+    return epub2txt(epub_file)
+
+# Créer une application 
+# 
+# Streamlit
 def main():
-    st.title("Chatbot sur l'Intelligence Artificielle")
-    st.write("Bienvenue ! Posez-moi toutes vos questions sur l'Intelligence Artificielle.")
-
-    fichier_texte = "votre_fichier_texte.txt"  # Remplacez par le chemin de votre fichier texte
-    with open(fichier_texte, "r", encoding="utf-8") as file:
-        donnees_texte = file.read()
-
-    phrases_pretraitees = preprocess(donnees_texte)
+    st.title("Chatbot sur la seconde guerre mondiale")
+    st.write("Bienvenue ! Posez-moi toutes vos questions sur la seconde guerre mondiale.")
+    
+    epub_file = "Encyclopédie de la Seconde .epub"
+    texte_extrait = extract_text_from_epub(epub_file)  # Modifier cette ligne
+    phrases_pretraitees = preprocess(texte_extrait)
 
     question_utilisateur = st.text_input("Posez une question :")
     if question_utilisateur:
